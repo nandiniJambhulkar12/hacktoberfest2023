c0a38f76158989c47d6291feb53e8b3ad3bc8d75
nandiniJambhulkar12 <147521548+nandiniJambhulkar12@users.noreply.github.com>
2023-10-21 17:34:16 +0530

    Create bubblesort.c

---
+++ bubblesort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void bubbleSort(int array[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                // Swap the elements
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int data[] = {64, 34, 25, 12, 22, 11, 90};
+    int size = sizeof(data) / sizeof(data[0]);
+    bubbleSort(data, size);
+    printf("Sorted array: \n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", data[i]);
+    }
+    return 0;
+}
